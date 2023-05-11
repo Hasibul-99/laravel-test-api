9d14f40610699c8bab1c9ba72da87c99d4384df0
hasibul-99 <hasibul_uap@outlook.com>
2023-05-11 15:59:03 +0600

    post request and validator implement

--- app/Http/Controllers/UsersApiController.php
+++ app/Http/Controllers/UsersApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\user;
+use Validator;
 
 class UsersApiController extends Controller
 {
@@ -14,6 +15,42 @@ class UsersApiController extends Controller
         } else {
             $users = User::find($id);
             return response()->json(["users"=>$users], 200);            
+        }
+    }
+
+    public function addUser(Request $request) {
+        if ($request->ismethod('post')) {
+            $data = $request->all();
+            // return $data;
+            $rules = [
+                'name'=> 'required',
+                'email'=> 'required|email|unique:users',
+                "password"=> 'required' 
+            ];
+
+            $customMessage = [
+                'name.required'=> 'Name is required',
+                'email.required'=> 'Email is required',
+                'email.email'=> 'Email must be a valid email',
+                'password.required'=> 'Password is required',
+            ];
+
+            $validator = Validator::make($data, $rules, $customMessage);
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 422);
+            }
+
+            $user = new User();
+            
+            $user->name = $data["name"];
+            $user->email = $data["email"];
+            $user->password = bcrypt($data['password']);
+
+            $user->save();
+
+            $message = "User Succesfully Added";
+            return response()->json(['message'=>$message], 201);
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 route::get('/users/{id?}', [UsersApiController::class, 'showUser']);
+route::post('/add-user', [UsersApiController::class, 'addUser']);
